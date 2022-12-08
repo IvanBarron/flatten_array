ea942fe1350d3a965463a335ef2ff7182bd1d6d4
Ivan BH <34050938+IvanBarron@users.noreply.github.com>
2022-12-08 19:39:52 +0000

    test(main) - adding a print example

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 
 import unittest
 import test
+from functions import flatten_array
 
 if __name__ == '__main__':
-    unittest.main(module=test)
+  
+  test_array = [[1,2],3,[4,5,[6,[7,8]],9,[10,11]]]
+  
+  result = flatten_array(test_array)
+  print(f"Test Array: {test_array}")
+  print(f"Result Array: {result}")
+  unittest.main(module=test)
